d0471cc4d93c77a0e59c6280e7afea919ff3a539
fernandomauri <maurif@rpi.edu>
2021-02-13 20:12:02 -0500

    Add files via upload

---
+++ getRSSFeeds.sh
@@ -0,0 +1,11 @@
+# Output RSS contents to a file in the XML directory using its link in string format
+# May require installing wget
+wget -O /path/to/xml/directory/file1 "https://www.uscourts.gov/news/rss"
+wget -O /path/to/xml/directory/file2 "https://www.cnbc.com/id/100003114/device/rss/rss.html"
+wget -O /path/to/xml/directory/file3 "https://www.cidrap.umn.edu/news/178636/rss"
+wget -O /path/to/xml/directory/file4 "https://feeds.npr.org/816541428/rss.xml"
+wget -O /path/to/xml/directory/file5 "https://tools.cdc.gov/api/v2/resources/media/403372.rss"
+wget -O /path/to/xml/directory/file6 "https://feeds.a.dj.com/rss/RSSMarketsMain.xml"
+wget -O /path/to/xml/directory/file7 "https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml"
+wget -O /path/to/xml/directory/file8 "https://rss.nytimes.com/services/xml/rss/nyt/US.xml"
+wget -O /path/to/xml/directory/file9 "http://news.google.com/news?q=covid-19&hl=en-US&sort=date&gl=US&num=100&output=rss"
